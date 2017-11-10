0aaef8b19dd9ae5aa25d25bc6178840016c1180a
Joshua Byrd <phocks@gmail.com>
2017-11-10 16:16:04 +1000

    Adding email validation

--- index.js
+++ index.js
@@ -2,30 +2,32 @@
 // It takes input from Chatfuel and sends it to Hearken
 
 const axios = require("axios"); // A small REST client
+const isemail = require("isemail");
 
 // Set the Hearken endpoint. This is exposed through the Hearken embeds
 // https://modules.wearehearken.com/abc/embed/361/share
 const hearkenUrl = "https://modules.wearehearken.com/abc/api/questions.js";
-
-// Some initial variables
-let isAnon = true, // Default to anonymous
-  sourceId = 361; // use ?sourceId=361 in Chatfuel to change this
 
 function handlePOST(req, res) {
   // We need query strings to set some parameters
   const query = req.query;
   const body = req.body;
 
-  sourceId = +query.sourceId || 361; // Make sourceId query string a number and provide fallback
-
   // Let's get user input here from the ChatFuel request
   const firstName = body["first name"] || "Firstname",
     lastName = body["last name"] || "Lastname",
-    email = body.email || "no@email.com",
     formResponse = body.formResponse || "No response",
     messengerUserId = body["messenger user id"] || "none-provided",
     preferAnonymous = body.PreferAnonymous || "Yes",
     location = body.location || "none-provided";
+
+  // Some initial variables
+  let isAnon = true, // Default to anonymous
+    sourceId = 361, // use ?sourceId=361 in Chatfuel to change this
+    email = body.email || "no@email.com";
+
+  // Make sourceId query string a number and provide fallback
+  sourceId = +query.sourceId || 361;
 
   // console.log(query);
 
@@ -44,11 +46,7 @@ function handlePOST(req, res) {
   }
 
   // Did they not want to provide an email address?
-  if (
-    email.toLowerCase() === "no" ||
-    email.toLowerCase() === "nup" ||
-    email.toLowerCase() === "nope"
-  ) {
+  if (!isemail.validate(email)) {
     email = "no@email.com;";
   }
 
